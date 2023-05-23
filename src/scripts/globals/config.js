baa97f47dd6aa5e5f186315859d5ada5219c4fb0
Johansyah Diaz <johansyah.diaz8@gmail.com>
2023-05-23 15:12:23 +0700

    gak bisa offline

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
-  CACHE_NAME: 'RestaurantCatalogue-V1',
+  CACHE_NAME: new Date().toISOString(),
   DATABASE_NAME: 'restaurant-catalogue-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurant',
