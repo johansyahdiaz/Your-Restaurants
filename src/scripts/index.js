be171f6bcecf39534dfc6c0954c6fc150f38570e
Johansyah Diaz <johansyah.diaz8@gmail.com>
2023-06-01 23:25:42 +0700

    memperbaiki revisi 2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,8 +6,6 @@ import swRegister from './utils/sw-register';
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
-
-
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
   drawer: document.querySelector('#navigationDrawer'),
@@ -22,4 +20,3 @@ window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
 });
-
